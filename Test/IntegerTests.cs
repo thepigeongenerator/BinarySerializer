e1b24efc49d3c856917d1df0a97159d539c10762
unset <quinnthepigeon@gmail.com>
2024-11-30 17:20:49 +0100

    modify unit tests a bit to test serialization seperately

--- Test/IntegerTests.cs
+++ Test/IntegerTests.cs
@@ -6,15 +6,20 @@ public class IntegerTests
 {
     // testing a lot of the integer types, first testing alternating bit patterns with the unsigned variants of the bits
     // then testing having all bits 'true'
+    public static IEnumerable<object[]> GetTestData()
+    {
+        yield return new object[] { (byte)0x55, (ulong)0x55 };
+        yield return new object[] { (ushort)0x5555, (ulong)0x5555 };
+        yield return new object[] { (uint)0x55555555, (ulong)0x55555555 };
+        yield return new object[] { (ulong)0x5555555555555555, (ulong)0x5555555555555555 };
+        yield return new object[] { (sbyte)-1, unchecked((ulong)-1) };
+        yield return new object[] { (short)-1, unchecked((ulong)-1) };
+        yield return new object[] { (int)-1, unchecked((ulong)-1) };
+        yield return new object[] { (long)-1, unchecked((ulong)-1) };
+    }
+
     [Theory]
-    [InlineData((byte)0x55)]
-    [InlineData((ushort)0x5555)]
-    [InlineData((uint)0x55555555)]
-    [InlineData((ulong)0x5555555555555555)]
-    [InlineData((sbyte)-1)]
-    [InlineData((short)-1)]
-    [InlineData((int)-1)]
-    [InlineData((long)-1)]
+    [MemberData(nameof(GetTestData))]
     public void Serialize_Deserialize_Equality<T>(T x) where T : struct
     {
         byte[] res = BinarySerializer.Serialize(x);
@@ -24,15 +29,18 @@ public class IntegerTests
     }
 
     [Theory]
-    [InlineData(default(byte))]
-    [InlineData(default(ushort))]
-    [InlineData(default(uint))]
-    [InlineData(default(ulong))]
-    public void Serialize_Deserialize_Size<T>(T x) where T : struct
+    [MemberData(nameof(GetTestData))]
+    public void Serialize<T>(T x, ulong val) where T : struct
     {
         int size = Marshal.SizeOf<T>();
-        byte[] res = BinarySerializer.Serialize(x);
+        byte[] expRes = new byte[size];
 
-        Assert.True(size == res.Length);
+        for (int i = 0; i < size; i++)
+        {
+            expRes[i] = (byte)(val >> (i * 8)); // casting to 'byte' truncates the rest
+        }
+
+        byte[] res = BinarySerializer.Serialize(x);
+        Assert.Equal(res, expRes);
     }
 }
