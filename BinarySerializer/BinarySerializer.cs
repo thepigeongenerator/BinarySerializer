925ec5fdba4de316617b3af62ff0953fe9a520ec
unset <quinnthepigeon@gmail.com>
2024-11-30 17:03:38 +0100

    move to a reflection-based system

--- BinarySerializer/BinarySerializer.cs
+++ BinarySerializer/BinarySerializer.cs
@@ -6,89 +6,36 @@ namespace ThePigeonGenerator.Util
 {
     public static class BinarySerializer
     {
-        // utility method for deserializing from a buffer pointer
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static unsafe T? Deserialize<T>(byte* pBuf)
-        {
-            return Marshal.PtrToStructure<T>((IntPtr)pBuf);
-        }
-
-        // utility method for serializing into a buffer pointer
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static unsafe void Serialize<T>(T obj, byte* pBuf) where T : struct
-        {
-            Marshal.StructureToPtr<T>(obj, (IntPtr)pBuf, false);
-        }
-
         // deserializes T from the specified buffer
+        [Obsolete("under development")]
         public static unsafe T? Deserialize<T>(byte[] buf) where T : struct
         {
             // I am assuming that the programmer is not stupid and will read from the correct type of buffer, hence me not checking the sizes
-            T? obj;
+            //T? obj;
 
             // get the array as a pointer
             fixed (byte* pBuf = &buf[0])
             {
                 // store the data of the pointer as the desired object
-                obj = Deserialize<T>(pBuf);
+                //obj = Deserialize<T>(pBuf);
             }
 
-            return obj;
+            return null;
         }
 
         // serializes the object into it's binary representation to the buffer
         public static unsafe byte[] Serialize<T>(T obj) where T : struct
         {
             // allocate a buffer with the size of T
-            byte[] buf = new byte[Marshal.SizeOf<T>()];
+            Type t = typeof(T);
+            int size = ReflectionUtil.GetStructureSize(t, obj);
+            byte[] buf = new byte[size];
 
             // get the array as a pointer (all arrays are a pointer)
             fixed (byte* pBuf = &buf[0])
             {
                 // store the structure to the pointer which points to the buffer
-                Serialize<T>(obj, pBuf);
-            }
-
-            return buf;
-        }
-
-        //
-        // array serialization / deserialization
-        //
-
-        // deserializes an array of T from the specified buffer
-        public static unsafe T?[] DeserializeArray<T>(byte[] buf)
-        {
-            int size = Marshal.SizeOf<T>();         // get how much memory T occupies
-            T?[] arr = new T?[buf.Length / size];   // allocate an array of T with the buffer's size divided by T's size to get the original T array size
-
-            for (int i = 0; i < arr.Length; i++)
-            {
-                // get a pointer using the input array's index to map everything correctly
-                fixed (byte* pBuf = &buf[i * size])
-                {
-                    arr[i] = Deserialize<T>(pBuf);
-                }
-            }
-
-            return arr;
-        }
-
-        // serializes an array of T to it's binary representation to the buffer
-        public static unsafe byte[] SerializeArray<T>(params T[] arr) where T : struct
-        {
-            // allocate a buffer with the size of T * the array's length
-            int size = Marshal.SizeOf<T>();             // get how much memory T occupies
-            byte[] buf = new byte[size * arr.Length];   // allocate a byte array the size of the T array
-
-            for (int i = 0; i < arr.Length; i++)
-            {
-                // get a pointer using the input array's index to map everything correctly
-                fixed (byte* pBuf = &buf[i * size])
-                {
-                    // store the structure to the pointer in the buffer
-                    Serialize<T>(arr[i], pBuf);
-                }
+                ReflectionUtil.SerializeStructure(t, obj, pBuf, size);
             }
 
             return buf;
---
+++ BinarySerializer/ReflectionUtil.cs
@@ -0,0 +1,87 @@
+#nullable enable
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace ThePigeonGenerator.Util;
+
+internal static class ReflectionUtil
+{
+    private const BindingFlags BINDING_FLAGS = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public;
+
+    // gets the size of an array datastructure
+    private static int GetArraySize(Type t, object obj)
+    {
+        // get the element type and check if it's valid
+        Type elmtType = t.GetElementType() ?? throw new NullReferenceException($"failed to get the element type of array {t.FullName}");
+        if (elmtType.IsArray) throw new NotSupportedException($"an error occurred when processing '{t.FullName}' => we do not support multi-dimensional array of this nature, due to their dynamic and thus unknown size");
+
+        int arrSize = GetStructureSize(elmtType, null); // get the size of the structure
+        int arrLen = ((Array)obj).Length;               // get the length of the array (note: in the case of a multi-dimensional array, this'll contain the amount of items across all dimensions)
+        return arrSize * arrLen;                        // multiply together to get the total size
+    }
+
+    // gets the size of a data structure, also serves as a means to validate that the data is in the correct format
+    public static int GetStructureSize(Type t, object? obj)
+    {
+        // if t is an array, get the size of the array
+        if (t.IsArray)
+            return GetArraySize(t, obj ?? throw new NullReferenceException($"tried to get the size of array '{t.FullName}', but the specified array was 'null'"));
+
+        // if t isn't a value type, we don't support it
+        if (t.IsValueType == false)
+            throw new NotSupportedException($"type {t.FullName} is not a value type and thus unsupported, due to it's unknown size");
+
+        // if t is a primitive type (int, float, bool), we can get the size
+        if (t.IsPrimitive)
+            return Marshal.SizeOf(t);
+
+        // otherwise, loop through the defined fields and call ourselves on these types
+        // sum this result
+        return (
+            from f in t.GetFields(BINDING_FLAGS)
+            select GetStructureSize(f.FieldType, f.GetValue(obj))
+        ).Sum();
+    }
+
+    private static unsafe void SerializeArray(Type t, object obj, byte* buf, int size)
+    {
+        Type elmtType = t.GetElementType() ?? throw new NullReferenceException($"failed to get the element type of '{t.FullName}'!");
+        Array arr = (Array)obj;
+
+        if (size % arr.Length != 0)
+            throw new Exception("the array's size was not devisable by the element count");
+
+        // get the element size by deviding the array size by the length
+        int elmtSize = size / arr.Length;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            object elmt = arr.GetValue(i) ?? throw new NullReferenceException($"failed to get an element with the index of '{i}' in array of type '{t.FullName}'");
+            SerializeStructure(elmtType, elmt, buf + (elmtSize * i), elmtSize);
+        }
+    }
+
+    public static unsafe void SerializeStructure(Type t, object obj, byte* buf, int size)
+    {
+        // if T is a primitive type (int, float, bool), we can just convert it straight away
+        if (t.IsPrimitive)
+            Marshal.StructureToPtr(obj, (IntPtr)buf, false);
+
+        // if T is an array, we can just serialize the array
+        if (t.IsArray)
+            SerializeArray(t, obj, buf, size);
+
+        // loop through each field in t, and call ourselves
+        int i = 0;
+        foreach (FieldInfo f in t.GetFields(BINDING_FLAGS))
+        {
+            // get the structure size of this
+            int s = GetStructureSize(f.FieldType, obj);
+            object val = f.GetValue(obj) ?? throw new NullReferenceException($"field '{f.FieldType.FullName}' in '{t.FullName}' is not allowed to be null!");
+            SerializeStructure(f.FieldType, val, buf + i, s);
+            i += size;
+        }
+    }
+}
