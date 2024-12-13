eaffc16b156d5620c547aac8eb581862ce1381bd
unset <quinnthepigeon@gmail.com>
2024-12-13 17:52:11 +0100

    add non-type parameter versions to the binary serializer

--- BinarySerializer/BinarySerializer.cs
+++ BinarySerializer/BinarySerializer.cs
@@ -5,13 +5,13 @@ namespace ThePigeonGenerator.Util
     public static class BinarySerializer
     {
         // serializes the object into it's binary representation to the buffer
-        public static unsafe byte[] Serialize<T>(T obj)
+        public static unsafe byte[] Serialize<T>(T obj) => Serialize(typeof(T), obj ?? throw new NullReferenceException("inputted object cannot be null!"));
+        public static unsafe byte[] Serialize(Type t, object obj)
         {
             if (obj == null)
                 throw new NullReferenceException("the object is not allowed to be null!");
 
             // allocate a buffer with the size of T
-            Type t = typeof(T);
             int size = ReflectionUtil.GetStructureSize(t, obj);
             byte[] buf = new byte[size];
 
@@ -26,17 +26,17 @@ namespace ThePigeonGenerator.Util
         }
 
         // deserializes T from the specified buffer
-        public static unsafe T? Deserialize<T>(byte[] buf)
+        public static unsafe T? Deserialize<T>(byte[] buf) => (T?)Deserialize(typeof(T), buf);
+        public static unsafe object? Deserialize(Type t, byte[] buf)
         {
             // I am assuming that the programmer is not stupid and will read from the correct type of buffer, hence me not checking the sizes
-            Type t = typeof(T);
-            T? obj;
+            object? obj;
 
             // get the array as a pointer
             fixed (byte* pBuf = &buf[0])
             {
                 // store the data of the pointer as the desired object
-                obj = (T?)ReflectionUtil.DeserializeStructure(t, pBuf);
+                obj = ReflectionUtil.DeserializeStructure(t, pBuf);
             }
 
             return obj;
